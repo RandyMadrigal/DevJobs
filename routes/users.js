14276cdb826484cedfba2f8b681e6bbb1f696193
Randy Madrigal <randymadrigalx12@gmail.com>
2023-10-14 17:47:45 -0400

    agregar validaciones via req.body

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const { body } = require("express-validator");
+
 const router = express.Router();
 
 const userController = require("../controllers/users");
 
-router.post("/createUser", userController.createUser); //login
+router.post(
+  "/createUser",
+  [
+    body("UserName").trim().notEmpty().isLength({ min: 5, max: 120 }),
+    body("UserNickName").trim().notEmpty().isLength({ min: 5, max: 120 }),
+    body("UserLastName").trim().notEmpty().isLength({ min: 5, max: 120 }),
+    body("UserAddress").notEmpty().isLength({ min: 5, max: 120 }),
+    body("UserEmail")
+      .trim()
+      .notEmpty()
+      .isEmail()
+      .withMessage("Not a valid Email")
+      .isLength({ min: 5, max: 120 }),
+    body("UserPhone")
+      .trim()
+      .notEmpty()
+      .withMessage("dont use space, max length 10")
+      .isLength({ min: 10, max: 15 }),
+    body("UserPassword").trim().notEmpty().isLength({ min: 5, max: 120 }),
+    body("ConfirmPassword").trim().notEmpty().isLength({ min: 5, max: 120 }),
+    body("UserImg").trim().notEmpty(),
+  ],
+  userController.createUser
+);
 
 exports.router = router;
