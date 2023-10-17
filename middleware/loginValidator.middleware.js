92ac82c520ea50d8040258d4eed11f4543071de5
luis fernandez <alejandroferllub@gmail.com>
2023-10-17 12:55:44 -0400

    fixed error in loginValidation

--- middleware/loginValidator.middleware.js
+++ middleware/loginValidator.middleware.js
@@ -1,5 +1,14 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+const secret = process.env.PRIVATE_KEY;
 
 export const loginValidator = (req, res, next) => {
+    if(!req.headers.authorization){
+        return res.status(400).json({ message: "Missing email or password" });
+    }
+
     const token = req.headers.authorization.split(" ")[1];
     const payload = jwt.verify(token, secret);
 
